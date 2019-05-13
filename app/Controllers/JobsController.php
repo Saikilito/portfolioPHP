0ae96599e9f607b53af8a35fb01dec33362636a6
Saikilito <kembert@gmail.com>
2019-05-12 23:50:05 -0400

    test the validator

--- app/Controllers/JobsController.php
+++ app/Controllers/JobsController.php
@@ -3,31 +3,51 @@
 namespace App\Controllers; 
 
 use App\Models\Job;
+use Respect\Validation\Validator as v ;
 
 class JobsController extends BaseController {
     public function addJobAction($request) {
 
+        $responseMessage = null;
+
         if($request->getMethod() == 'POST')
         {
             $postData = $request->getParsedBody();
-            $job = new Job();
-            $job->title = $postData['title'];
-            $job->description = $postData['description'];
-            $job->visible = true ;
-            $job->save();
+
+            $jobValidator = v::key('title', v::stringType()->notEmpty()) 
+                            ->key('description', v::stringType()->notEmpty()); // true  
+            try{
+                $jobValidator->assert($postData);
+                
+                if($jobValidator->validate($postData))
+                {
+                    $job = new Job();
+                    $job->title = $postData['title'];
+                    $job->description = $postData['description'];
+                    $job->visible = true ;
+                    $job->save();
+                }
+
+                $responseMessage = 'Saved!';
+            }   
+            catch(\Exception $e) {
+                $responseMessage = $e->getMessage();
+            }
         }
         
-        // if(!empty($_POST))
-        // {
-            // $job = new Job();
-            // $job->title = $_POST['title'];
-            // $job->description = $_POST['description'];
-            // $job->visible = true ;
-            // $job->save();
-        // }
-        
-        return $this->renderHTML('addJob.twig');
+        return $this->renderHTML('addJob.twig',[
+            'responseMessage' => $responseMessage
+        ]);
     }
 }
+
+// if(!empty($_POST))
+// {
+    // $job = new Job();
+    // $job->title = $_POST['title'];
+    // $job->description = $_POST['description'];
+    // $job->visible = true ;
+    // $job->save();
+// }
 
 ?>
